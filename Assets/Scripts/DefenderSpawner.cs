1985829576a2cfa31c77965abe0a1050f213ea6f
unknown <ilonnaa12@gmail.com>
2021-06-01 17:41:02 +0300

    Added deferenders to the menu to place players on the field

---
+++ Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour
+{
+    Defender defender;
+    public void SetSelectedDefender(Defender selectedDefenders) 
+    {
+        defender = selectedDefenders;
+    }
+
+    private void OnMouseDown()
+    {
+        Vector2 worldPos = GetSquareClicked();
+    }
+
+    private Vector2 GetSquareClicked()
+    {
+        Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Debug.Log("X = " + clickPos.x.ToString() + " Y = " + clickPos.y.ToString());
+        Vector2 worldPos = Camera.main.ScreenToViewportPoint(clickPos);
+        Debug.Log("X = " + worldPos.x.ToString() + " Y = " + worldPos.y.ToString());
+        return new Vector2();
+    }
+
+}
